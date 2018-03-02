9f6e109abce274c5e400dc612c59627578e67eae
consultoria@carlosdelfino.eti.br <consultoria@carlosdelfino.eti.br>
2018-03-02 19:43:32 -0300

    update for new diretory structure

--- test.ts
+++ test.ts
@@ -1,10 +1,4 @@
 /// <reference path="./android.d.ts" />
-/// <reference path="./ios.d.ts" />
-
-class IOSTest extends NSObject {
-}
-
-let pointer = new interop.Pointer();
 
 class AndroidTest extends java.lang.Object {
     public do() {
